057786a3063cbe258c094284a42d62d3f44d1802
cupshir <christopherupshir@ccvonline.com>
2017-08-18 15:46:44 -0700

    This is a jered commit...derpa derp

--- RockWeb/Plugins/church_ccv/SafetySecurity/DPSOffenderImport.ascx.cs
+++ RockWeb/Plugins/church_ccv/SafetySecurity/DPSOffenderImport.ascx.cs
@@ -7,6 +7,7 @@ using System.Web.UI.WebControls;
 
 using church.ccv.SafetySecurity.Model;
 using Microsoft.AspNet.SignalR;
+using OfficeOpenXml;
 using Rock;
 using Rock.Attribute;
 using Rock.Data;
