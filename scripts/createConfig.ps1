eb39034dff83bba61de1b2436411140f14a530d5
Vann Gutierrez <developer@ccvonline.com>
2017-09-17 09:30:44 -0700

    adding scripts to allow for connection strings to be created

---
+++ scripts/createConfig.ps1
@@ -0,0 +1,15 @@
+﻿Param([string]$resourcegroup ='devccvcloud1', [string]$webapp = 'rockwebapp', [string]$datasource , [string]$catalog,[string]$user, [string]$userPass)
+$connectionStrConfig = @"
+<?xml version="1.0"?>
+<connectionStrings>
+         <add name="RockContext" connectionString="Data Source=$datasource;Initial Catalog=$catalog;
+             Integrated Security=False;User ID=$user;Password=$userPass;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True;
+             ApplicationIntent=ReadWrite;MultiSubnetFailover=False; MultipleActiveResultSets=true" providerName="System.Data.SqlClient"/>
+  </connectionStrings>
+"@
+$connectionStrConfig | out-file .\web.ConnectionStrings.config -Force
+$kudufile = get-item .\web.ConnectionStrings.config
+ipmo .\kudufunctions.psm1 -Force
+$rockwebPubProfile = Get-PublishingProfileCredentials -resourceGroupName $resourcegroup -webAppName $webapp
+$rockwebAuthHeader = Get-KuduApiAuthorisationHeaderValue -resourceGroupName $resourcegroup -webAppName $webapp
+Upload-FileToWebApp -resourceGroupName $resourcegroup -webAppName $webapp -localPath $kudufile.FullName -kuduPath $kudufile.name
---
+++ scripts/kudufunctions.psm1
@@ -0,0 +1,42 @@
+﻿function Get-PublishingProfileCredentials($resourceGroupName, $webAppName){
+ 
+    $resourceType = "Microsoft.Web/sites/config"
+    $resourceName = "$webAppName/publishingcredentials"
+ 
+    $publishingCredentials = Invoke-AzureRmResourceAction -ResourceGroupName $resourceGroupName -ResourceType $resourceType -ResourceName $resourceName -Action list -ApiVersion 2015-08-01 -Force
+ 
+       return $publishingCredentials
+}
+ 
+function Get-KuduApiAuthorisationHeaderValue($resourceGroupName, $webAppName){
+ 
+    $publishingCredentials = Get-PublishingProfileCredentials $resourceGroupName $webAppName
+ 
+    return ("Basic {0}" -f [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $publishingCredentials.Properties.PublishingUserName, $publishingCredentials.Properties.PublishingPassword))))
+}
+
+
+
+function Upload-FileToWebApp($resourceGroupName, $webAppName, $slotName = "", $localPath, $kuduPath){
+
+    $kuduApiAuthorisationToken = Get-KuduApiAuthorisationHeaderValue $resourceGroupName $webAppName $slotName
+
+    if ($slotName -eq ""){
+
+        $kuduApiUrl = "https://$webAppName.scm.azurewebsites.net/api/vfs/site/wwwroot/$kuduPath"
+
+    }
+
+    else{
+
+        $kuduApiUrl = "https://$webAppName`-$slotName.scm.azurewebsites.net/api/vfs/site/wwwroot/$kuduPath"
+
+    }
+
+    $virtualPath = $kuduApiUrl.Replace(".scm.azurewebsites.", ".azurewebsites.").Replace("/api/vfs/site/wwwroot", "")
+
+    Write-Host " Uploading File to WebApp. Source: '$localPath'. Target: '$virtualPath'..."  -ForegroundColor DarkGray
+    
+    Invoke-RestMethod -Uri $kuduApiUrl  -Headers @{"Authorization"=$kuduApiAuthorisationToken;"If-Match"="*"} -Method PUT -InFile $localPath -ContentType "multipart/form-data"
+
+}
